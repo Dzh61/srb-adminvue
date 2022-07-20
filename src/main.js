3a328b301d4500e43fd4634fefd6cddce5eeb32c
RubyOnly <463923648@qq.com>
2022-07-20 14:52:58 +0800

    xzx

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios"
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import router from "@/router";
+
+
+Vue.prototype.$http=axios
+axios.defaults.baseURL = 'http://localhost:8080/'
+Vue.use(ElementUI);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 注册路由器
+  router: router
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:80',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {'^/api': ''}
+      }
+    }
+
+    // proxy: 'http://localhost:9999'
+  }
 })
